53d4c8ef6aa43e4cfee975fe11f18aaa7ac269b2
sujit1234s <sawant.sujit2003@gmail.com>
2026-04-27 09:00:26 +0530

    Variable information in js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 console.log('This is the Message')
 console.warn('This is Warning')
 console.error('this is Error')
+
+var a=10
+console.log(a)
+
+var a=54
+var b=4
+console.log(a%b)
+
+alert("Data Leak")
+confirm("Are you an Adult ?")
+
