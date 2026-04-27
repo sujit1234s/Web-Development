c425ca445fd10d5dad18fbefc40c94443dbb2ba8
sujit1234s <sawant.sujit2003@gmail.com>
2026-04-27 10:02:42 +0530

    Adding Looping Statments

--- script.js
+++ script.js
@@ -1,71 +1,29 @@
-console.log('This is the Message')
-console.warn('This is Warning')
-console.error('this is Error')
 
-var a=10
-console.log(a)
+var a = 0
+ while(a<5)
+ {
+    console.log(a)
+    a++
+ }
 
-var a=54
-var b=4
-console.log(a%b)
+ var a = 0
+ while(a<5)
+ {
+    console.log(a)
+    a--
+ }
 
-alert("Data Leak")
+for (var a =0;a<5;a++)
+{
+    console.log("Hello")
+}
 
-var ans = confirm("Are you an Adult ?")
-console.log(ans)
+var a = 10
+var b = '10'
 
-var a = confirm("are you sure ?")
-console.log(a)
-
-var a =prompt("Enter your Name")
-console.log(a)
-
-alert("Message")
-confirm("confirm hai naa Bhai ?")
-
-var user = prompt("enter your name")
-console.log("User name is",user)
-
-//JS Datatypes
-
-//-Premetive Datatypes (Single Data)
-   //- Number: 1,2,3,6.66,3.14
-
-   var a=10
-   console.log(a)
-
-   //-string:'a',"a","sujit",'sujit'
-
-   var a='sujit'
-   console.log(a)
-
-   //-boolean: true , false
-   //-underfined
-   //-null
-   //-symbol
-   //-NAN:Not a Number
-
-   var a=10
-   var b='sujit'
-   console.log(a*b)
-
-   //+ : Addition/Concatination
-   //- : Sub
-   //* : Mult
-   /// : div
-
-   //Condition 
- var a = 1
-   if(10>5){
-        console.log("correct")
-   }else{
-    console.log("Not correct")
-   }
-
-
-
-
-
- 
-
-
+if(a===b){
+    console.log("Sahi Hai")
+}
+else{
+    console.log("Galat hai")
+}
