2d738f25f05403f27c2f3afa8300e1e804b357ac
sujit1234s <sawant.sujit2003@gmail.com>
2026-04-27 11:42:57 +0530

    Adding Function and Array

--- script.js
+++ script.js
@@ -47,3 +47,49 @@ function dance()
 sing()
 dance()
 hey()
+
+function abc()
+{
+    console.log("Hello")
+    return 20
+}
+
+var a= abc()
+
+console.log(a)
+
+var abc = function(){
+    console.log("hELLO")
+}
+abc()
+
+var abc = () =>{
+    console.log("Hello")
+}
+
+abc()
+  
+const groom = "Selmon"
+const bride = "Kat"
+
+console.log(groom ,"Weds" ,bride)
+
+var a = [10,20,30,40]
+
+console.log(a)
+
+var a=[10,'sujit',6.66,true]
+console.log(a)
+
+var a=[10,20,30,40]
+//.     0  1  2  3
+console.log(arr[5])
+
+a.push(99)
+console.log(a)
+
+a.pop(10)
+console.log(a)
+
+console.log(a.length)
+
